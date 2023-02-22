77b7bd60e96dce7d53ac5163ee7b821302265e7d
Chenweichu <chenweichu@bytedance.com>
2023-02-22 22:16:12 +0800

    migrate

--- sample/src/main/java/com/getbase/floatingactionbutton/sample/MainActivity.java
+++ sample/src/main/java/com/getbase/floatingactionbutton/sample/MainActivity.java
@@ -1,17 +1,17 @@
 package com.getbase.floatingactionbutton.sample;
 
-import com.getbase.floatingactionbutton.FloatingActionButton;
-import com.getbase.floatingactionbutton.FloatingActionsMenu;
-
-import android.app.Activity;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.OvalShape;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends Activity {
+import com.getbase.floatingactionbutton.FloatingActionButton;
+import com.getbase.floatingactionbutton.FloatingActionsMenu;
+
+public class MainActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
